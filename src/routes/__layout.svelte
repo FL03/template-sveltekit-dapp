14a30a227ded8f0236100b3aca0c3cc1b9f6cf1b
FL03 <jo3mccain@icloud.com>
2022-08-03 10:38:20 -0500

    Update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,34 +1,46 @@
 <script>
-	import Footer from '$lib/components/footer/Footer.svelte';
-	import Navbar from '$lib/components/nav/Navbar.svelte';
-	import '../app.css';
-	let brand  = {
-		abbr: "scsys",
-		name: "Scattered-Systems",
-		slug: "scattered-systems",
-		tagline: "Welcome to the future of internet-based experiences",
-		type: "LLC",
-		website: "https://scattered-systems.com"
-	}
-	let views = [
-		{
-			id: 0,
-			key: "homepage",
-			label: "Home",
-			endpoint: "/"
-		}
-	]
+  import Footer from '$lib/components/footer/Footer.svelte';
+  import Navbar from '$lib/components/nav/Navbar.svelte';
+  import '../app.css';
+
+  let data = {
+    brand: {
+      abbr: "svelte",
+      name: "SvelteKit",
+      slug: "sveltekit",
+      tagline: "Develop beautiful, dynamic web applications with ease leveraging SvelteKit",
+      type: "LLC",
+      website: ""
+    },
+    views: [
+      {
+          id: 0,
+          key: "homepage",
+          label: "Home",
+          endpoint: "/"
+      },
+      {
+          id: 1,
+          endpoint: "/about",
+          label: "About"
+      },
+      {
+          id: 2,
+          endpoint: "/todo",
+          label: "ToDo"
+      }
+    ]
+  }
 </script>
 
 <div class="flex">
-	<div class="flex-auto min-h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
-		<div class="container mx-auto">
-			<Navbar brand={brand} />
-			<main class="flex-auto mt-3 py-3 rounded ">
-				<slot />
-			</main>
-			<Footer brand={brand}/>
-		</div>
-		
-	</div>
+  <div class="flex-auto min-h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
+    <div class="container mx-auto">
+        <Navbar brand={data.brand} views={data.views} />
+        <main class="flex-auto mt-3 py-3 rounded">
+            <slot />
+        </main>
+        <Footer brand={data.brand}/>
+    </div>
+  </div>
 </div>
